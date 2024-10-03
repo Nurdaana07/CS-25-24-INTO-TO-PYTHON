50f98eed5cf1f9366a0ade72bae7e98cd7bd6f42
Nurdaana07 <onurdaana@gmail.com>
2024-10-03 16:37:03 +0600

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,23 @@
+#задание use while and for
+#while : cont, break
+#for: cont, break
+  #and,or
+
+  #table: 2,4,8
+
+# num = 0
+# while num < 5:
+#     num += 1
+#     if num == 3:
+#         continue
+#     else:
+#         print('now the number is',num)
+
+
+num = 0
+while num < 5:
+    num += 1
+    if num == 3:
+        break
+    else:
+        print('now the number is',num)
